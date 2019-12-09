10a5b5ce7b42d9b148cc737e075529f12e125821
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-08 22:10:49 -0500

    changed cost to price

--- shopcart.js
+++ shopcart.js
@@ -1,9 +1,16 @@
-const items = [
+const items =[
     {item:"milk",price:2.50,id:0},
     {item:"bread",price:1.50,id:0},
     {item:"eggs",price:1.99,id:0,},
     {item:"rice",price:4.50,id:0},
     {item:"beans",price:1.75,id:0},
     {item:"sugar",price:5.50,id:0},
-
 ];
+
+items.forEach(function(ele){
+    // console.log(ele);
+    let div = document.createElement('div');
+    div.innerHTML=`<h3>${ele.item}</h3>$${ele.price}`;
+    div.style.border=
+    document.body.appendChild(div);
+})
