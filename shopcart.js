91658ae1718973e6eb532f5181a01d8520128770
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-08 21:26:49 -0500

    created an array, no output though

--- shopcart.js
+++ shopcart.js
@@ -1,9 +1,9 @@
 const items = [
-    {item:"milk",price:2.50},
-    {item:"bread",price:1.50},
-    {item:"eggs",price:1.99},
-    {item:"rice",price:4.50},
-    {item:"beans",price:1.75},
-    {item:"sugar",price:5.50},
+    {item:"milk",price:2.50,id:0},
+    {item:"bread",price:1.50,id:0},
+    {item:"eggs",price:1.99,id:0,},
+    {item:"rice",price:4.50,id:0},
+    {item:"beans",price:1.75,id:0},
+    {item:"sugar",price:5.50,id:0},
 
-]
+];
