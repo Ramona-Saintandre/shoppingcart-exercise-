1af01b484ef17bff1b097229fe10982a80512375
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-08 20:17:24 -0500

    setup files, and added css to the page

---
+++ shopcart.js
@@ -0,0 +1,9 @@
+const items = [
+    {item:"milk",price:2.50},
+    {item:"bread",price:1.50},
+    {item:"eggs",price:1.99},
+    {item:"rice",price:4.50},
+    {item:"beans",price:1.75},
+    {item:"sugar",price:5.50},
+
+]
