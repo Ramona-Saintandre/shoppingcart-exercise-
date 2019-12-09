343400b547ed43f2f4eee879505e7bdfc6e62561
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-08 22:39:48 -0500

    created cart, and added click evt

--- shopcart.js
+++ shopcart.js
@@ -1,16 +1,62 @@
-const items =[
-    {item:"milk",price:2.50,id:0},
-    {item:"bread",price:1.50,id:0},
-    {item:"eggs",price:1.99,id:0,},
-    {item:"rice",price:4.50,id:0},
-    {item:"beans",price:1.75,id:0},
-    {item:"sugar",price:5.50,id:0},
+const items = [{
+        item: "milk",
+        price: 2.50,
+        id: 0
+    },
+    {
+        item: "bread",
+        price: 1.50,
+        id: 0
+    },
+    {
+        item: "eggs",
+        price: 1.99,
+        id: 0,
+    },
+    {
+        item: "rice",
+        price: 4.50,
+        id: 0
+    },
+    {
+        item: "beans",
+        price: 1.75,
+        id: 0
+    },
+    {
+        item: "sugar",
+        price: 5.50,
+        id: 0
+    },
 ];
-
-items.forEach(function(ele){
+const cart = {};
+items.forEach(function (ele) {
     // console.log(ele);
     let div = document.createElement('div');
-    div.innerHTML=`<h3>${ele.item}</h3>$${ele.price}`;
-    div.style.border=
+    div.innerHTML = `<h3>${ele.item}</h3>$${ele.price}`;
+    div.style.border = '1px solid #fff';
+    div.style.display = 'inline-block';
+    div.style.width = '100px';
+    // creating a function inside of function
+    div.addEventListener('click',function(){
+let name = ele.item.toLocaleLowerCase();
+console.log(name);
+    
+    if(!cart[name]){
+cart[name]={
+    name:ele.item,
+    price:ele.price,
+    qty:1,
+    subtotal:function(){
+        return this.price * this.qty
+    }
+}}else{
+    cart[name].qty;
+}
+    })
     document.body.appendChild(div);
 })
+
+// adding the event listner 
+
+
